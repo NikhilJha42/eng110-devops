5538cf5b2bdb2fdedab6ce52c61a8300aa8ddc78
Nikhil Jha <njha@spartaglobal.com>
2022-04-25 09:29:10 +0100

    Added work in progress for automating reverse proxy

--- starter-code/starter-code/provisioning.sh
+++ starter-code/starter-code/provisioning.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
 
-# ensuring base operating system is working
 sudo apt-get update -y
+
 sudo apt-get upgrade -y
 
-# installing and enabling nginx
 sudo apt-get install nginx -y
 
 sudo systemctl start nginx
 
 sudo systemctl enable nginx
 
+cd app/app
+
+sudo apt-get install python-software-properties -y
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo apt-get update -y
+
+npm install
+
+rm -rf /etc/nginx/sites-available/default
+
+npm start &
+
+# ensuring base operating system is working
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+
+# installing and enabling nginx
+#sudo apt-get install nginx -y
+
+#sudo systemctl start nginx
+
+#sudo systemctl enable nginx
+
 # installing nodejs (V6)
-sudo apt-get install python-software-properties
+#sudo apt-get install python-software-properties
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-sudo apt-get install nodejs -y
+#sudo apt-get install nodejs -y
+
+#sudo rm -rf /etc/nginx/sites-available/default
+
+#sudo mv app/app/default_folder/default /etc/nginx/sites-available/
+
+#sudo systemctl restart nginx
+
+#cd app/app
+
+#npm install
+
+#npm start &
+
